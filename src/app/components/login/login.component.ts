0a6e51b578c61be0eb8c705f50c0660a617cc874
Gerard <gp_rocks@hotmail.com>
2018-11-11 23:57:55 +0000

    Update redirect url

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -7,16 +7,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
-
-  constructor(private router: Router) { }
+  constructor(private router: Router) {}
 
   showSpinner = false;
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   goToLogin(): void {
     // tslint:disable-next-line:max-line-length
-    window.location.href = 'https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22D752&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fexternal-login%2F&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800';
+    // window.location.href = 'https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22D752&redirect_uri=http%3A%2F%2Flocalhost%3A4200%2Fexternal-login%2F&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800';
+    window.location.href =
+      // tslint:disable-next-line:max-line-length
+      'https://www.fitbit.com/oauth2/authorize?response_type=token&client_id=22D752&redirect_uri=https%3A%2F%2Fweeklydeficit.azurewebsites.net%2Fexternal-login%2F&scope=activity%20heartrate%20location%20nutrition%20profile%20settings%20sleep%20social%20weight&expires_in=604800';
   }
 }
-
